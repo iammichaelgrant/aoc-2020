43c8a04edd7e11a678f23c74b228aae1aa151c9c
mgrant <michael.grant@youview.com>
2020-12-19 10:55:00 +0000

    formatting

--- src/day_5.clj
+++ src/day_5.clj
@@ -4,27 +4,27 @@
 (def example-1 "FBFBBFFRLR")
 
 (defn power [n exp]
-     (apply * (repeat exp n)))
+  (apply * (repeat exp n)))
 
 (defn calc-row [sections low-char]
-     (loop [acc 0
-            sections sections]
-          (if (empty? sections)
-               acc
-               (recur (+ acc
-                         (if (= (first sections)
-                                low-char)
-                              0
-                              (power 2 (dec (count sections)))))
-                      (rest sections)))))
+  (loop [acc 0
+         sections sections]
+    (if (empty? sections)
+      acc
+      (recur (+ acc
+                (if (= (first sections)
+                       low-char)
+                  0
+                  (power 2 (dec (count sections)))))
+             (rest sections)))))
 
 (defn seat-number [seat-code]
-     (let [rows (subs seat-code 0 7)
-           cols (subs seat-code 7 10)
-           row  (calc-row rows \F)
-           col (calc-row cols \L)]
-          (+ (* row 8)
-             col)))
+  (let [rows (subs seat-code 0 7)
+        cols (subs seat-code 7 10)
+        row  (calc-row rows \F)
+        col (calc-row cols \L)]
+    (+ (* row 8)
+       col)))
 
 ;Part 1
 (->> input
@@ -32,11 +32,12 @@
      (apply max))
 
 (defn find-missing-seat [seats]
-     (loop [seats seats] (if (= (+ (first seats) 1)
-                                (second seats))
-                              (recur (rest seats))
-                              (+ (first seats)
-                                 1))))
+  (loop [seats seats]
+    (if (= (+ (first seats) 1)
+           (second seats))
+      (recur (rest seats))
+      (+ (first seats)
+         1))))
 
 ;Part 2
 ; 1
